5e875c69f37d3a568750e55d6753d26b468c5c1d
TanviDeshpande18 <tanvi@epidote.in>
2025-07-12 12:35:02 +0530

    validation report html

--- validation_html_generator.py
+++ validation_html_generator.py
@@ -38,9 +38,9 @@ class VAL_HTMLGenerator:
             # Find specific logos by name
             for file in files:
                 if 'dna' in file['name'].lower():
-                    logos['dna'] = self._download_logo(file)
+                    logos['dna'] = self._download_file(file,"image")
                 elif 'company' in file['name'].lower():
-                    logos['company'] = self._download_logo(file)
+                    logos['company'] = self._download_file(file,"image")
             
             return logos
             
@@ -48,13 +48,17 @@ class VAL_HTMLGenerator:
             print(f"Error getting logos: {str(e)}")
             return None
         
-    def get_data_images(self, project_name):
+    def get_val_data_images_and_json(self, project_name):
         try:
             valdata_folder = self.handler.config.get_validation_data_folder_id()
             data_images = {}
+            json_data = {}
             
             # Query for both logos
-            query = f"parents='{valdata_folder}' and mimeType contains 'image/' and trashed=false"
+            query = (
+                f"'{valdata_folder}' in parents and "
+                "(mimeType contains 'image/' or mimeType = 'application/json') and trashed=false"
+            )
             results = self.handler.service.files().list(
                 q=query,
                 fields="files(id, name, mimeType)",
@@ -62,6 +66,7 @@ class VAL_HTMLGenerator:
             ).execute()
             
             files = results.get('files', [])
+            # print("FILES", files)
             if not files:
                 print("No logos found in logo folder")
                 return None
@@ -71,54 +76,46 @@ class VAL_HTMLGenerator:
             for file in files:
                 if file['name'].lower().startswith(project_name.lower()):
                     if 'adapter_content' in file['name'].lower():
-                        data_images['adapter_content'] = self._download_logo(file)
+                        data_images['adapter_content'] = self._download_file(file,"image")
                     elif 'average_base_calling_accuracy' in file['name'].lower():
-                        data_images['average_base_calling_accuracy'] = self._download_logo(file)
+                        data_images['average_base_calling_accuracy'] = self._download_file(file,"image")
                     elif 'basecoverage' in file['name'].lower():
-                        data_images['basecoverage'] = self._download_logo(file)
+                        data_images['basecoverage'] = self._download_file(file,"image")
                     elif 'per_base_quality' in file['name'].lower():
-                        data_images['per_base_quality'] = self._download_logo(file)
+                        data_images['per_base_quality'] = self._download_file(file,"image")
+                    elif file['name'].lower().endswith('validation_data.json'):
+                        json_data = self._download_file(file,"json")
             
-            return data_images
+            print("json_data", json_data)
+            return data_images,json_data
             
         except Exception as e:
             print(f"Error getting logos: {str(e)}")
             return None
         
 
-    def _download_logo(self, file):
+    def _download_file(self, file,what):
         """Helper method to download and convert logo to base64."""
         try:
             request = self.handler.service.files().get_media(fileId=file['id'])
-            logo_bytes = BytesIO()
-            downloader = MediaIoBaseDownload(logo_bytes, request)
+            file_bytes = BytesIO()
+            downloader = MediaIoBaseDownload(file_bytes, request)
             
             done = False
             while not done:
                 _, done = downloader.next_chunk()
             
             # Return as base64 data URL
-            logo_data = base64.b64encode(logo_bytes.getvalue()).decode()
-            return f"data:{file['mimeType']};base64,{logo_data}"
+            if what == 'image':
+                logo_data = base64.b64encode(file_bytes.getvalue()).decode()
+                return f"data:{file['mimeType']};base64,{logo_data}"
+            elif what == 'json':
+                file_bytes.seek(0)
+                return json.load(file_bytes)    
             
         except Exception as e:
             print(f"Error downloading logo {file['name']}: {str(e)}")
             return None        
-
-    def read_validation_json(self, file_id):
-        try:
-            request = self.handler.service.files().get_media(fileId=file_id)
-            fh = BytesIO()
-            downloader = MediaIoBaseDownload(fh, request)
-            done = False
-            while not done:
-                status, done = downloader.next_chunk()
-            fh.seek(0)
-            data = json.load(fh)
-            return data
-        except Exception as e:
-            print(f"Error reading JSON from Drive: {e}")
-            return None
 
 
     def generate_html(self, report_data):
@@ -126,10 +123,9 @@ class VAL_HTMLGenerator:
         try:
             # Get both logos
             logos = self.get_logos()
-            data_images = self.get_data_images(report_data['project'])
 
-            # Get validation json data
-            validation_json_data = self.read_validation_json(str(report_data['project']) + '_validation_data.json')
+            # Get data images and validation json with all values for the report                                                                                     
+            val_data_images, val_json = self.get_val_data_images_and_json(report_data['project'])
 
             # Read template
             with open(self.template_path, 'r', encoding='utf-8') as f:
@@ -143,10 +139,10 @@ class VAL_HTMLGenerator:
             dna_img = logos['dna'] if logos and 'dna' in logos else ''
 
             # Get data images
-            adapter_content_img = data_images['adapter_content'] if data_images and 'adapter_content' in data_images else ''
-            average_base_calling_accuracy_img = data_images['average_base_calling_accuracy'] if data_images and 'average_base_calling_accuracy' in data_images else ''
-            basecoverage_img = data_images['basecoverage'] if data_images and 'basecoverage' in data_images else ''
-            per_base_quality_img = data_images['per_base_quality'] if data_images and 'per_base_quality' in data_images else '' 
+            adapter_content_img = val_data_images['adapter_content'] if val_data_images and 'adapter_content' in val_data_images else ''
+            average_base_calling_accuracy_img = val_data_images['average_base_calling_accuracy'] if val_data_images and 'average_base_calling_accuracy' in val_data_images else ''
+            basecoverage_img = val_data_images['basecoverage'] if val_data_images and 'basecoverage' in val_data_images else ''
+            per_base_quality_img = val_data_images['per_base_quality'] if val_data_images and 'per_base_quality' in val_data_images else '' 
             
 
             with open(self.stylesheet) as f:
@@ -157,11 +153,24 @@ class VAL_HTMLGenerator:
                 css = css,
                 dna_img = dna_img,
                 company_logo=company_logo,
+
+                # Data numbers and images
+                run = val_json['Run'],
+                flow_cell = val_json['Flow_cell'],
+                run_date = val_json['Run_date'],
+                tech = val_json['Technician'],
+                genome_size = val_json['Genome_size_Kb'],
+                bases_sequenced = val_json['Number_of_bases_sequenced_Mb'],
+                coverage = val_json['Coverage_of_sequenced_data'],
+                alignment_stats = val_json['Alignment_statistics'].replace('\n', '<br><br>'),
+
                 adapter_content_img=adapter_content_img,
                 average_base_calling_accuracy_img=average_base_calling_accuracy_img,
                 basecoverage_img=basecoverage_img,
                 per_base_quality_img=per_base_quality_img,
-                
+                val_json=val_json,
+
+
                 title=report_data['title'],
                 project=report_data['project'],
                 analysis_type=report_data['analysis_type'],
